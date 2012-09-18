f6124fecda31d94b43d4f0ad461db1acb2f01200
twh <twh@twh-VirtualBox.(none)>
2012-09-18 17:41:42 +0800

    update new blog project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a9e060082d560de5eb14c23f1190c7f465d6cfa1f58d99fb15961a398060f6b3482c1a3a44a260ccba38a54d01e3ef45e323d1e7026df02c6ef4e03483715f0e'
